739c46f3a188286187da3396f9684ecac2a10d3e
njonusiene <jonusiene.n@gmail.com>
2023-11-24 19:16:27 +0200

    1 prisedimas

--- script.js
+++ script.js
@@ -14,24 +14,24 @@ document.addEventListener("DOMContentLoaded", function() {
     // tl.to('#homeSlide', { x: "0%", duration: 1.2, ease: "power2.out" }, "-=0.5");
 
     // Animation for homepage2
-    tl.to('.homepage2', { x: "0%", duration: 1.5, ease: "power2.out" }, "-=0.5");
+    tl.to('.homepage2', { x: "0%", duration: 2, ease: "power2.out" }, "-=0.5");
 });
 
 document.addEventListener("DOMContentLoaded", function() {
     // Animation timeline
-    var tl = gsap.timeline({ repeat: 1, yoyo: true });
+    var tl = gsap.timeline({ repeat: -1, yoyo: true });
 
     // Set initial rotation
-    tl.set('#swingingImg', { transformOrigin: '50% 90%', rotation: 0 });
+    tl.set('#swingingImg', { transformOrigin: '40% 90%', rotation: 0 });
 
     // Add swinging animation
-    tl.to('#swingingImg', { rotation: 10, duration: 0.5, ease: "power1.inOut" });
-    tl.to('#swingingImg', { rotation: -10, duration: 1, ease: "power1.inOut" });
+    tl.to('#swingingImg', { rotation: 8, duration: 0.5, ease: "power1.inOut" });
+    tl.to('#swingingImg', { rotation: -8, duration: 1, ease: "power1.inOut" });
 });
 
 document.addEventListener("DOMContentLoaded", function() {
     // Animation timeline
-    var tl = gsap.timeline({ repeat: 1, yoyo: true });
+    var tl = gsap.timeline({ repeat: -1, yoyo: true });
 
     // Set initial rotation
     tl.set('#swingingImg1', { transformOrigin: '90% 50%', rotation: 0 });
@@ -41,6 +41,5 @@ document.addEventListener("DOMContentLoaded", function() {
     tl.to('#swingingImg1', { rotation: 10, duration: 1, ease: "power1.inOut" });
 });
 
-//use the defaults
-gsap.to(element, {duration: 1, scrambleText: "Catlana Ragdoll Cats | Adorable Ragdoll kittens in Idaho"});//or customize things:
+
 
